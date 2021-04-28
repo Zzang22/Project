c5e666c486115115e3611e6917b195474825b466
Hwang DaHye <hwangdahye90@gmail.com>
2021-04-29 00:58:42 +0900

    JS home container fade out

--- main.js
+++ main.js
@@ -31,6 +31,16 @@ btn_contact.addEventListener('click', () =>{
    scrollIntoView('#contact');
 });
 
+
+//Make home slowly fadeto transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () =>{
+   home.style.opacity =1 - window.scrollY / homeHeight;
+});
+
+
+
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior:"smooth"});
