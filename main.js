4c6ed18c7b159d87a0e8c32d9005c4ec3c9b13f2
Hwang DaHye <hwangdahye90@gmail.com>
2021-04-28 20:49:49 +0900

    javascript home button contactme, navbar

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 'use strict'
 
-// scroll에 따라 navbar를 투명하게, 진하게 만들기 
+// navbar의 height 만큼 scroll 되면, navbar를 진하게 만들기 
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
+   // console.log(window.scrollY); 스크롤 position
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     }else{
         navbar.classList.remove('navbar--dark');
     }
 });
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu =document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) =>{
+    console.log(event.target.dataset.link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link ==null){
+        return; 
+    }
+    scrollIntoView(link);
+});
+
+//contact me 
+const btn_contact = document.querySelector(".home__contact");
+btn_contact.addEventListener('click', () =>{
+   scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+}
